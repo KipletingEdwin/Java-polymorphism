14bd03c1addaa4e2ff4ede6c26b1c9e10dcf9f83
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-02 12:19:38 +0300

    finalize polymorphism

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,20 @@ public class Main {
     public static void main(String[] args) {
         //polymorphism = greek word for poly-"many", morph-"form"
         //The ability of an object to identify as more than one type
+        Car car = new Car();
+        Bicycle bicycle = new Bicycle();
+        Boat boat = new Boat();
+
+       Vehicle[] racers = {car, bicycle, boat};
+
+//       car.go();
+//       bicycle.go();
+//       boat.go();
+
+        for (Vehicle x : racers){
+            x.go();
+        }
+
 
     }
 }
