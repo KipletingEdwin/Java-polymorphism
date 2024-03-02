99d31e5ab677ed32b3a88c7ca1c9b44ba3131529
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-03-02 11:57:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //polymorphism = greek word for poly-"many", morph-"form"
+        //The ability of an object to identify as more than one type
+
+    }
+}
